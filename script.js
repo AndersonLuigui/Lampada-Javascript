1983ba4f6a9e771828833466a314735147776312
AndersonLuigui <alns1536@hotmail.com>
2023-01-06 22:00:12 -0300

    ERRO

---
+++ script.js
@@ -0,0 +1,18 @@
+const turnOn = document.getElementById ('turnOn');
+const turnOff = document.getElementById ('turnOff');
+const lamp  = document.getElementById ('lamp');
+
+
+function lampOn() {
+    lamp.scr = './img/ligada.jpg'
+}
+
+
+function lampOff() {
+    lamp.scr = './img/desligada.jpg'
+}
+
+
+
+turnOn.addEventListener ( 'click', lampOn );
+turnOff.addEventListener ( 'click', lampOff );
